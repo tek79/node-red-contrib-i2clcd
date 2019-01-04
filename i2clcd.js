e8225360d3769df1287325f9d00659d4e317a9b1
tek79 <ken.ellefson79@gmail.com>
2019-01-03 22:20:16 -0600

    Update i2clcd.js

--- i2clcd.js
+++ i2clcd.js
@@ -16,7 +16,8 @@ module.exports = function(RED) {
       this.LCD_BUS = parseInt(config.bus);
       console.log("LCD node init @ i2c addr:" + this.LCD_ADDR);
       console.log("LCD node init @ i2c bus:" + this.LCD_BUS);
-      lcd = new LCD("/dev/i2c-"+this.LCD_BUS,this.LCD_ADDR);
+      //lcd = new LCD("/dev/i2c-"+this.LCD_BUS,this.LCD_ADDR);
+      lcd = new LCD(this.LCD_BUS,this.LCD_ADDR);
       initLCD();
           
       this.on('input', function(msg) {
