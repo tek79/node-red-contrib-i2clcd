e913b94e0eb9633df608453b87776d45f6f672b9
tek79 <ken.ellefson79@gmail.com>
2019-01-06 17:30:18 -0600

    Update i2clcd.js

--- i2clcd.js
+++ i2clcd.js
@@ -47,23 +47,23 @@ module.exports = function(RED) {
          }
 
          if (msg.topic.localeCompare("line1") == 0) {
-             lcd.setCursor(0,0)
-             lcd.print(msg.payload);
+             //lcd.setCursor(0,0)
+             lcd.println(msg.payload,1);
          }
 
          if (msg.topic.localeCompare("line2") == 0) {
-            lcd.setCursor(0,1)
-             lcd.print(msg.payload);
+            //lcd.setCursor(0,1)
+             lcd.println(msg.payload,2);
          }
          
          if (msg.topic.localeCompare("line3") == 0) {
-            lcd.setCursor(0,2)
-             lcd.print(msg.payload);
+            //lcd.setCursor(0,2)
+             lcd.println(msg.payload,3);
          }
          
          if (msg.topic.localeCompare("line4") == 0) {
-            lcd.setCursor(0,3)
-             lcd.print(msg.payload);
+            //lcd.setCursor(0,3)
+             lcd.println(msg.payload,4);
          }
          node.send(msg); //pass message through
       });
